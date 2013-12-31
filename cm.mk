7068ef8f2612db999e8a3318b2966c8cc47b20bb
Shareef Ali <shareefalis@cyanogenmod.org>
2013-12-30 19:41:01 -0600

    hlte: fixup build.prop
    Change-Id: Ia217a8986d588cda6606e7c8e66a2637e36f9583

--- cm.mk
+++ cm.mk
@@ -9,7 +9,7 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=hltespr TARGET_DEVICE=hltespr BUILD_FINGERPRINT="samsung/hltespr/hltespr:4.2.2/JDQ39/L720VPUAMDL:user/release-keys" PRIVATE_BUILD_DESC="hltespr-user 4.2.2 JDQ39 L720VPUAMDL release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=hltespr TARGET_DEVICE=hltespr BUILD_FINGERPRINT="samsung/hltespr/hltespr:4.3/JSS15J/N900PVPUBMJ4:user/release-keys" PRIVATE_BUILD_DESC="hltespr-user 4.3 JSS15J N900PVPUBMJ4 release-keys"
 
 
 PRODUCT_NAME := cm_hltespr
