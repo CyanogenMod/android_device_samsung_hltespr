5b96f375204bbf9747674e16288b82ad8acb2adb
Ethan Chen <intervigil@gmail.com>
2014-09-05 14:52:40 -0700

    hltespr: Clean up copy paste error
    Change-Id: Iac964b9998e4969c24e39083b809522d558e2cb8

--- init/init_hlte.c
+++ init/init_hlte.c
@@ -56,7 +56,6 @@ void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *boar
 
     if (strstr(bootloader, "N900P")) {
         /* hltespr */
-        cdma_properties("1");
         property_set("ro.build.fingerprint", "samsung/hltespr/hltespr:4.4.2/KOT49H/N900PVPUCNAB:user/release-keys");
         property_set("ro.build.description", "hltespr-user 4.4.2 KOT49H N900PVPUCNAB release-keys");
         property_set("ro.product.model", "SM-N900P");
