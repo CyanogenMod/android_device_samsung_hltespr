8726ee881c9711df8fe207d45b91cb4cd49b282f
repo Shareef Ali <shareefalis@gmail.com>
2014-12-23 16:15:34 -0600

    hlte: kill dupe in device tree.
    Change-Id: I31383e5c9cac260aa238e45234416d7e54d6de76

--- init/init_hlte.c
+++ init/init_hlte.c
@@ -76,6 +76,5 @@ void cdma_properties(char cdma_sub[], char op_numeric[], char op_alpha[])
     property_set("ro.cdma.home.operator.alpha", op_alpha);
     property_set("ro.telephony.default_cdma_sub", cdma_sub);
     property_set("ro.telephony.default_network", "10");
-    property_set("ro.telephony.ril.config", "newDriverCallU,newDialCode");
     property_set("telephony.lteOnCdmaDevice", "1");
 }
